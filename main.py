e534291ef7583b13893b92c65dcbd8362d1aab90
Viktorius S <viktorius.tori@gmail.com>
2023-03-04 20:10:37 +0000

    create function to calculate day and time

--- main.py
+++ main.py
@@ -1,70 +1,94 @@
 # This entrypoint file to be used in development. Start by reading README.md
 
-# from time_calculator import add_time
-# from unittest import main
+from time_calculator import add_time
+from unittest import main
 
 
-# print(add_time("11:06 PM", "2:02"))
+print(add_time("11:06 PM", "2:02"))
 
 # Run unit tests automatically
-# main(module='test_module', exit=False)
+main(module='test_module', exit=False)
 
 # -----------------------------------------------------------------------------
 # MY TESTER CODE
 # -----------------------------------------------------------------------------
+# min_added_to_hour = 0
+# def calc_minute_left (min) :
+#   min_left = min - int(min/60)
+#   if (min_left < 60) : return min
+#   elif min_left >=60 : 
+#     min_added_to_hour += 1
+#     calc_minute_left(min_left)
+  
 def add_time1(start, duration, day_input = None) :
-  print(day_input)
+  import math
 
-  # prepare variable for time calculation
-  final_time = { 'hour' : '', 'min' : '', 'timing' : '', 'day' : '' }
-  start_t = start.replace(' ',':').split(':')
-  dur_t   = duration.split(':')
-
+  # prepare variable for day & time calculation
+  timing_list = ['AM', 'PM']
+  day_list    = ['Sunday', 'Monday', 'tuesday', 'Wednesday', 'Thursday', 'Friday', 'saturDay']
+  final_time  = { 'hour' : '', 'min' : '', 'timing' : '', 'day' : '', 'days_after' : '' }
+  start_t     = start.replace(' ',':').split(':')
+  dur_t       = duration.split(':')
+  min_before = 0; min_after = 0; hour_before = 0; hour_after = 0; day_before = 0; day_after = 0;
+  
   # prepare value assignment
-  hour_start   = int(start_t[0])
-  min_start    = int(start_t[1])
-  timing_start = start_t[2]
-  
-  hour_dur    = int(dur_t[0])
-  min_dur     = int(dur_t[1])
+  hour_before   = int(start_t[0]); hour_dur = int(dur_t[0]);
+  min_before    = int(start_t[1]); min_dur  = int(dur_t[1]) 
+  timing_before = start_t[2];      timing_after = ''
+  day_before    = day_input if day_input is not None else None
+  day_after    = ''
+  many_days_after = ''
 
-  # calculate hour, minute, and timing
-  hour_add   = hour_start + hour_dur
-  min_add    = min_start  + min_dur
-
-  min        = min_add if min_add < 60   else abs(min_add - 60)
-  min_over   = 0 if min_add < 60 else int(min_add/60)  
-  hour_add  += min_over
+  # calculate minutes
+  min_add              = min_before + min_dur
+  min_left             = int(min_add%60)
+  min_convert_to_hour  = int(min_add/60)
+  # ceil_min_convert     = math.ceil(min_add/60)/
+  min_after            = 0 if min_left == 60 else (min_left - 60) if min_left > 60 else min_left
+  min_added_to_hour    = min_convert_to_hour if min_add >= 60 else 0
   
-  hour       = hour_add if hour_add <= 12 else abs(hour_add - 12)
-  
-  timing     = timing_start
-  if   (timing == 'PM' and hour_add >= 12) : timing = 'AM'
-  elif (timing == 'AM' and hour_add >= 12) : timing = 'PM'
+  # calculate hours
+  hour_add             = hour_before + hour_dur + min_added_to_hour
+  hour_left            = int(hour_add%24)
+  hour_convert_to_day  = int(hour_add/24)
+  # ceil_hour_convert    = math.ceil(hour_add/24)
+  hour_after           = (hour_left - 12) if hour_left > 12 else hour_left
+  hour_added_to_day    = hour_convert_to_day if hour_add >= 24 else 0
 
+  # calculate timing
+  if   hour_left < 12: timing_after = f'{timing_before}'
+  elif timing_before == 'AM' and hour_left >= 12 and hour_left < 24 : timing_after = 'PM'
+  elif timing_before == 'PM' and hour_left >= 12 and hour_left < 24 : timing_after = 'AM'
+  else : timing_after = f"{timing_list[ timing_list.index(timing_before) + hour_added_to_day - len(timing_list) ]}"
+  
   # calculate day
-  day_list   = ['Sunday', 'Monday', 'tuesday', 'Wednesday', 'Thursday', 'Friday', 'saturDay']
-  day        = 0 if hour_add <=24 else int(hour_add/24)
-  day_idx    = (7 - 1 - day_list.index(day_input))  and day > 7 else 0
+  day_index_before    = day_list.index(day_before) if day_before is not None else 0
+  if (
+      (min_add >= 60 and hour_add >= 24) or 
+      (hour_left >= 12 and timing_before == 'PM' and timing_after == 'AM')
+    )  : day_add = hour_added_to_day + 1
+  else : day_add = hour_added_to_day
   
-    
+  day_add_index       = int( (day_index_before + day_add) %7)
+  day_index_after     = day_add_index - len(day_list)
+  day_after           = day_list[ day_index_after ] if day_before is not None else day_before
   
-  final_time['hour']   = f'{hour}'
-  final_time['min']    = f'{min}' if min >=10 else f'0{min}'
-  final_time['timing'] = timing
-  final_time['day']    = day
-
-  format = f"{str(final_time['hour'])}:{str(final_time['min'])} {str(final_time['timing'])}"
-  if day_input is not None :
-    pass
-
-    
-  format += f', {day_list[day_idx]}' if day_input is not None else ''
-  format += " (next day)" if day == 1 else f" ({day} days latter" if day > 1 else ''
+  # calculate after day
+  many_days_after = ' (next day)' if day_add == 1 else f' ({day_add} days later)' if day_add > 1 else ''
+  
+  # --------------------------------------------------------------------------------
+  # format the output
+  final_time['min']        = f'{min_after}' if min_after >=10 else f'0{min_after}'
+  final_time['hour']       = f'{hour_after}'
+  final_time['timing']     = f' {timing_after}'
+  final_time['day']        = f' {day_after}'
+  final_time['days_after'] = f'{many_days_after}'
+  
+  format = f"{str(final_time['hour'])}:{str(final_time['min'])}{str(final_time['timing'])}"
+  format += f",{final_time['day']}" if day_before is not None else ''
+  format += f"{many_days_after}"
   
   return format
-
-
   
 def test() :
   start1, duration1          = ("3:30 PM", "2:12")
@@ -107,12 +131,11 @@ def test() :
     {'checking 11': add_time1(start11, duration11, day11) == a11},
     {'checking 12': add_time1(start12, duration12, day12) == a12},
   ]
-
+  
+# ----------------------------------------------------------------------------------------
   for c in checking:
     print(f'{c}\n')
 
-  # print(add_time1("11:06 PM", "2:02"))
-  print( add_time1(start9, duration9, day9) )
-  print(a9)
+  print(add_time1("11:06 PM", "2:02"))
   
-test()
+# test()
--- time_calculator.py
+++ time_calculator.py
@@ -1,3 +1,66 @@
-def add_time(start, duration):
-  new_time = start + duration
-  return new_time
+def add_time(start, duration, day_input = None):
+
+  # prepare variable for day & time calculation
+  timing_list = ['AM', 'PM']
+  day_list    = ['Sunday', 'Monday', 'tuesday', 'Wednesday', 'Thursday', 'Friday', 'saturDay']
+  final_time  = { 'hour' : '', 'min' : '', 'timing' : '', 'day' : '', 'days_after' : '' }
+  start_t     = start.replace(' ',':').split(':')
+  dur_t       = duration.split(':')
+  min_before = 0; min_after = 0; hour_before = 0; hour_after = 0; day_before = 0; day_after = 0;
+  
+  # prepare value assignment
+  hour_before   = int(start_t[0]); hour_dur = int(dur_t[0]);
+  min_before    = int(start_t[1]); min_dur  = int(dur_t[1]) 
+  timing_before = start_t[2];      timing_after = ''
+  day_before    = day_input if day_input is not None else None
+  day_after    = ''
+  many_days_after = ''
+
+  # calculate minutes
+  min_add              = min_before + min_dur
+  min_left             = int(min_add%60)
+  min_convert_to_hour  = int(min_add/60)
+  min_after            = 0 if min_left == 60 else (min_left - 60) if min_left > 60 else min_left
+  min_added_to_hour    = min_convert_to_hour if min_add >= 60 else 0
+  
+  # calculate hours
+  hour_add             = hour_before + hour_dur + min_added_to_hour
+  hour_left            = int(hour_add%24)
+  hour_convert_to_day  = int(hour_add/24)
+  hour_after           = (hour_left - 12) if hour_left > 12 else hour_left
+  hour_added_to_day    = hour_convert_to_day if hour_add >= 24 else 0
+
+  # calculate timing
+  if   hour_left < 12: timing_after = f'{timing_before}'
+  elif timing_before == 'AM' and hour_left >= 12 and hour_left < 24 : timing_after = 'PM'
+  elif timing_before == 'PM' and hour_left >= 12 and hour_left < 24 : timing_after = 'AM'
+  else : timing_after = f"{timing_list[ timing_list.index(timing_before) + hour_added_to_day - len(timing_list) ]}"
+  
+  # calculate day
+  day_index_before    = day_list.index(day_before) if day_before is not None else 0
+  if (
+      (min_add >= 60 and hour_add >= 24) or 
+      (hour_left >= 12 and timing_before == 'PM' and timing_after == 'AM')
+    )  : day_add = hour_added_to_day + 1
+  else : day_add = hour_added_to_day
+  
+  day_add_index       = int( (day_index_before + day_add) %7)
+  day_index_after     = day_add_index - len(day_list)
+  day_after           = day_list[ day_index_after ] if day_before is not None else day_before
+  
+  # calculate after day
+  many_days_after = ' (next day)' if day_add == 1 else f' ({day_add} days later)' if day_add > 1 else ''
+  
+  # --------------------------------------------------------------------------------
+  # format the output
+  final_time['min']        = f'{min_after}' if min_after >=10 else f'0{min_after}'
+  final_time['hour']       = f'{hour_after}'
+  final_time['timing']     = f' {timing_after}'
+  final_time['day']        = f' {day_after}'
+  final_time['days_after'] = f'{many_days_after}'
+  
+  format = f"{str(final_time['hour'])}:{str(final_time['min'])}{str(final_time['timing'])}"
+  format += f",{final_time['day']}" if day_before is not None else ''
+  format += f"{many_days_after}"
+  
+  return format
